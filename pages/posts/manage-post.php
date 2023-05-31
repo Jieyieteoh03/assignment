93cdbf136f27d3d2604d388f155fcc1138802374
Teoh Jie Yie <jieyiet2003@gmail.com>
2023-05-31 09:17:36 +0800

    update

--- pages/posts/manage-post.php
+++ pages/posts/manage-post.php
@@ -1,8 +1,8 @@
 <?php
-  // if( !isUserLoggedIn()){
-  //   header("Location: /");
-  //   exit;
-  // }
+  if( !isUserLoggedIn()){
+    header("Location: /");
+    exit;
+  }
   // load database
   $database = connectToDB();
 
